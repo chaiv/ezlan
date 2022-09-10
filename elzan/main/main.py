ac48e6783a06c0fa6ce6ff748f3c15dd70cc3764
vital <vital@LAPTOP-BANG055V.fritz.box>
2022-09-10 11:17:02 +0200

    # WARNING: head commit changed in the meantime
    master branch

---
+++ elzan/main/main.py
@@ -0,0 +1,17 @@
+'''
+Created on 10.09.2022
+
+@author: vital
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
